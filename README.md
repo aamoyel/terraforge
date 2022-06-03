2fc2ff3a7c70388a38e1c1e275574ad1ca9f977a
Alan Amoyel <alanamoyel06@gmail.com>
2022-06-03 12:38:43 +0200

    Init repo

---
+++ README.md
@@ -0,0 +1,131 @@
+# Terraform VM Forge
+<div id="top"></div>
+
+
+
+<!-- TABLE OF CONTENTS -->
+<details open>
+  <summary>Table of Contents</summary>
+  <ol>
+    <li>
+      <a href="#about-the-project">About The Project</a>
+      <ul>
+        <li><a href="#built-with">Built With</a></li>
+      </ul>
+    </li>
+    <li>
+      <a href="#getting-started">Getting Started</a>
+      <ul>
+        <li><a href="#prerequisites">Prerequisites</a></li>
+        <li><a href="#installation">Installation</a></li>
+      </ul>
+    </li>
+    <li><a href="#usage">Usage</a></li>
+    <li><a href="#license">License</a></li>
+    <li><a href="#contact">Contact</a></li>
+  </ol>
+</details>
+</br>
+
+
+
+<!-- ABOUT THE PROJECT -->
+## About The Project
+
+This project allows you to create fresh VMs instances on Proxmox.
+Tools like Cloud-Init, HashiCorp Vault, phpIPAM and PowerDNS are integrated to automate and simplify VMs configurations. Features are:
+- Dynamic password generation for root user with Vault.
+- Get the first IP addr of subnet of your choise with phpIPAM
+- Configurate VM network, hostname and root password at start.
+- Create dns record with your VM name and IP addr.
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+
+
+### Built With
+
+* [Proxmox](https://www.proxmox.com)
+* [Cloud-Init](https://cloud-init.io/)
+* [HashiCorp Vault](https://www.vaultproject.io/)
+* [phpIPAM](https://phpipam.net/)
+* [PowerDNS](https://www.powerdns.com/)
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+
+
+<!-- GETTING STARTED -->
+## Getting Started
+
+This is an example of how you may give instructions on setting up your project locally.
+To get a local copy up and running follow these simple example steps.
+
+### Prerequisites
+
+You need to have an instance of :
+* Proxmox
+* HashiCorp Vault
+* phpIPAM
+* PowerDNS
+
+After that, you need to have a cloud-init template in Proxmox and [terraform binary](https://learn.hashicorp.com/tutorials/terraform/install-cli) on a PC/server.
+
+### Installation
+
+1. Clone the repo :
+   ```sh
+   git clone https://github.com/aamoyel/tf-forge
+   ```
+2. Configure your APIs credentials and URLs in terraform.tfvars.json.
+3. Init your terraform directory and install deps.
+   ```sh
+   terraform init
+   ```
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+
+
+<!-- USAGE EXAMPLES -->
+## Usage
+
+1. Setup your VMs in vm-instances.json, exemple here :
+   ```json
+    {
+      "my-vm-name": {
+        "cores": "2",
+        "sockets": "1",
+        "memory": "2048",
+        "network": "vmbr1",
+        "subnet": "PRD",
+        "domain": "amoyel.loc"
+      }
+    }
+   ```
+2. Create the instance :
+   ```sh
+   terraform apply
+   ```
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+
+
+<!-- LICENSE -->
+## License
+
+Distributed under the MIT License. See `LICENSE.txt` for more information.
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+
+
+<!-- CONTACT -->
+## Contact
+
+Alan Amoyel - [@AlanAmoyel](https://twitter.com/AlanAmoyel)
+
+Project Link: [https://github.com/aamoyel/tf-forge](https://github.com/aamoyel/tf-forge)
+
+<p align="right">(<a href="#top">back to top</a>)</p>
---
+++ main.tf
@@ -0,0 +1,195 @@
+######################################## Requirements
+terraform {
+  required_providers {
+    vault = {
+      source = "hashicorp/vault"
+      version = "3.6.0"
+    }
+    proxmox = {
+      source = "Telmate/proxmox"
+      version = "2.9.10"
+    }
+    phpipam = {
+      source = "lord-kyron/phpipam"
+      version = "1.2.12"
+    }
+    powerdns = {
+      source = "pan-net/powerdns"
+      version = "1.5.0"
+    }
+  }
+}
+
+
+
+
+######################################## Variables
+variable "vault_address" {}
+variable "vault_role_id" {}
+variable "vault_secret_id" {}
+variable "phpipam_app_id" {}
+variable "phpipam_endpoint" {}
+variable "phpipam_password" {}
+variable "phpipam_username" {}
+variable "proxmox_api_url" {}
+variable "proxmox_api_token_id" {}
+variable "proxmox_api_token_secret" {}
+variable "powerdns_api_key" {}
+variable "powerdns_server_url" {}
+
+
+
+
+######################################## Providers configurations
+provider "vault" {
+  address = var.vault_address
+  auth_login {
+    path = "auth/approle/login"
+    parameters = {
+      role_id   = var.vault_role_id
+      secret_id = var.vault_secret_id
+    }
+  }
+}
+
+provider "phpipam" {
+  app_id   = var.phpipam_app_id
+  endpoint = var.phpipam_endpoint
+  password = var.phpipam_password
+  username = var.phpipam_username
+}
+
+provider "proxmox" {
+  pm_api_url = var.proxmox_api_url
+  pm_api_token_id = var.proxmox_api_token_id
+  pm_api_token_secret = var.proxmox_api_token_secret
+}
+
+provider "powerdns" {
+  server_url = var.powerdns_server_url
+  api_key = var.powerdns_api_key
+}
+
+
+
+
+######################################## Resources
+# Gen root password for vm instance
+resource "vault_generic_endpoint" "gen_passwd" {
+  for_each = local.vm_instances
+  path = "/gen/password"
+  disable_read = true
+  disable_delete = true
+  ignore_absent_fields = true
+  write_fields = ["value"]
+  data_json = <<EOT
+{
+  "symbols": "0",
+  "length": "18"
+}
+EOT
+}
+# Write password in Vault
+resource "vault_generic_secret" "root_passwd" {
+  for_each = local.vm_instances
+  path = "vms/${each.key}"
+  delete_all_versions = "true"
+  data_json = <<EOT
+{
+  "root-password": "${vault_generic_endpoint.gen_passwd[each.key].write_data.value}"
+}
+EOT
+}
+
+
+# Get ID of phpIPAM section
+data "phpipam_section" "section" {
+  name = "Home"
+}
+# Get ID of the subnet
+data "phpipam_subnet" "subnet" {
+  for_each = local.vm_instances
+  section_id = data.phpipam_section.section.id
+  description = each.value.subnet
+}
+# Get the first available address and reserve it. Note that we use ignore_changes here to ensure that we don't end up re-allocating this address on future Terraform runs.
+resource "phpipam_first_free_address" "new_ip" {
+  for_each = local.vm_instances
+  subnet_id = data.phpipam_subnet.subnet[each.key].subnet_id
+  hostname = "${each.key}.${each.value.domain}"
+  description = "Managed by Terraform"
+  lifecycle {
+    ignore_changes = [
+      subnet_id,
+      ip_address,
+      last_seen
+    ]
+  }
+}
+# Get the subnet gateway IP
+data "phpipam_address" "gateway" {
+  for_each = local.vm_instances
+  address_id = data.phpipam_subnet.subnet[each.key].gateway_id
+}
+
+
+# Create vm on Proxmox
+resource "proxmox_vm_qemu" "vm" {
+  for_each          = local.vm_instances
+  name              = each.key
+  target_node       = "hyp-pve01"
+  clone             = "tmpl-debian11"
+  cores             = each.value.cores
+  sockets           = each.value.sockets
+  cpu               = "host"
+  memory            = each.value.memory
+  scsihw            = "virtio-scsi-pci"
+  agent             = 1
+  network {
+    model           = "virtio"
+    bridge          = each.value.network
+  }
+  disk {
+    slot    = 0
+    size    = "16G"
+    storage = "VMFS2"
+    type    = "scsi"
+  }
+  # CloudInit config
+  ciuser = "root"
+  cipassword = "${vault_generic_endpoint.gen_passwd[each.key].write_data.value}"
+  ipconfig0 = "ip=${resource.phpipam_first_free_address.new_ip[each.key].ip_address}/24,gw=${data.phpipam_address.gateway[each.key].ip_address}"
+  connection {
+    type        = "ssh"
+    user        = "root"
+    private_key = "${file("~/.ssh/id_rsa")}"
+    host        = self.ssh_host
+  }
+  # Change hostname and DNS config
+  provisioner "remote-exec" {
+    inline = [
+      "hostnamectl set-hostname ${each.key}.${each.value.domain}",
+      "echo -n '    dns-nameservers 172.16.2.254\n    dns-search amoyel.loc' >> /etc/network/interfaces.d/50-cloud-init",
+      "shutdown -r +0"
+    ]
+  }
+}
+
+
+# Add A record in PowerDNS
+resource "powerdns_record" "vm_record" {
+  for_each = local.vm_instances
+  zone     = "${each.value.domain}."
+  name     = "${each.key}.${each.value.domain}."
+  type     = "A"
+  ttl      = 300
+  records  = ["${resource.phpipam_first_free_address.new_ip[each.key].ip_address}"]
+}
+
+
+
+
+######################################## VMs
+locals {
+  vm_instances = jsondecode(file("./vm-instances.json"))
+}
